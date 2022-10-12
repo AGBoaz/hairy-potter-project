aaa8637a19ea4f507dff8b261839874c883ddf9e
Grace Boaz <anna.g.boaz@gmail.com>
2022-10-12 10:21:44 -0500

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+// Imports go first
+import { makePottery } from './potteryWheel.js';
+
+
+// Make 5 pieces of pottery at the wheel
+let mug = makePottery('bowl','1 lb','4 inches');
+let mug2 = makePottery('vase','1.5 lb',' 1 foot');
+let mug3 = makePottery('mug','0.25 lb','4 inches');
+let mug4 = makePottery('yard knome','3 lb','3 feet');
+let mug5 = makePottery('brick','4 lb','6 inches');
+
+// Fire each piece of pottery in the kiln
+
+
+// Determine which ones should be sold, and their price
+
+
+// Invoke the component function that renders the HTML list
+
+
+
---
+++ src/scripts/potteryWheel.js
@@ -0,0 +1,11 @@
+let primaryKey = 1;
+
+export const makePottery = (shape,weight,height) => {
+    let pot = {
+        id: primaryKey,
+        shape: shape,
+        weight: weight,
+        height: height
+    }
+    primaryKey++;
+}
